78461349b0d66ee562b467212dde7a8259572263
Sumit0009 <100469632+sumitkumarsharm@users.noreply.github.com>
2024-07-13 03:29:59 +0000

    how  to declear the variable and how many datatype in javascript hhow ro check the type  of variable

---
+++ Day-1_Variable_And_DataType/variable.js
@@ -0,0 +1,67 @@
+// Activity 1 Variable decelaration
+// task-1 declare a variable using var assign it a number and log the value to the console
+var Num = 999;
+console.log(Num);
+
+// task-2 declare a variable using let assign it a string and log the value to the console
+
+let myName = "Sumit sharma";
+console.log(myName);
+
+// Activity 2 Constant decelaration
+// task-3 declare a variable using const assign it a boolean value and log the value to the console
+const myID = 21451254;
+console.log(myID);
+
+// Activity 3 Data Types
+// task-4 create a variable of different data type(number string boolan Object Array) and log each type of variable usinng typeOf variable
+// string
+let mySurname = "sharma";
+console.log(typeof mySurname);
+
+// number
+let myIDs = 21451254;
+console.log(typeof myIDs);
+
+// objects
+let myAddress = {
+  city: "Delhi",
+  state: "Delhi",
+  country: "India",
+};
+console.log(typeof myAddress);
+
+// Array
+let myFriends = ["Sumit", "Rahul", "Rajat", "Rahul"];
+console.log(typeof myFriends);
+
+// boolean
+let myBool = true;
+console.log(typeof myBool);
+
+// null
+let myNull = null;
+console.log(typeof myNull);
+
+// undefined 
+let myUndefined = undefined;
+console.log(typeof myUndefined);
+
+// synbol
+let mySymbol = Symbol("mySymbol");
+console.log(typeof mySymbol);
+
+// bigInt
+let myBigInt = 1234567890123456789n;
+console.log(typeof myBigInt);
+
+// float
+let myFloat = 123.456;
+console.log(typeof myFloat);
+
+
+// NaN
+let myNaN = NaN;
+console.log(typeof myNaN);
+
+
