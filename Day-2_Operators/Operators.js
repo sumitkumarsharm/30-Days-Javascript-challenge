833e3ee2de0e372bc4cce948179d800b05b5c96c
Sumit0009 <100469632+sumitkumarsharm@users.noreply.github.com>
2024-07-15 10:04:53 +0000

    30 day's challeneg of day 2 operators

---
+++ Day-2_Operators/Operators.js
@@ -0,0 +1,36 @@
+// Activity - 1  ARITHMETIC OPERATOR
+// TASK-1 Write a program to Add two numbers and log  the result to the console
+let first_number = 79;
+let second_number = 45;
+let result = first_number + second_number;
+console.log(result);
+
+// TASK-1 Write a program to subttract two numbers and log  the result to the console
+let first_number_1 = 79;
+let second_number_1 = 45;
+let result_1 = first_number_1 - second_number_1;
+console.log(result_1);
+
+// TASK-1 Write a program to multily two numbers and log  the result to the console
+let first_number_2 = 79;
+let second_number_2 = 45;
+let result_2 = first_number_2 * second_number_2;
+console.log(result_2);
+
+// TASK-1 Write a program to divide two numbers and log  the result to the console
+let first_number_3 = 79;
+let second_number_3 = 45;
+let result_3 = first_number_3 / second_number_3;
+console.log(result_3);
+
+// Activity - 1  ASSIGNMENTS OPERATOR
+// TASK-6 Write a program to use += operartor and add the value and console it into lof
+let first_number_4 = 20;
+first_number_4 += 2;
+console.log(first_number_4);
+
+// TASK-6 Write a program to use -= operartor and subtract the value and console it into lof
+
+let first_number_5 = 20;
+first_number_5 -= 2;
+console.log(first_number_5);
