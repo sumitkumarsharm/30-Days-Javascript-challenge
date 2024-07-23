29e358924acb17fcca78de46d8e59ea4e4b2c3ca
Sumit0009 <100469632+sumitkumarsharm@users.noreply.github.com>
2024-07-23 11:40:46 +0000

    30 Days challenge of day 4

---
+++ Day-4_Loops/loops.js
@@ -0,0 +1,32 @@
+// Activites and Tasks
+
+// Activity - 1
+// Task-1 write the program to print number from 1 to 10 using For loop
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// Task-2 write the program to print mulyiplication of 5 using While loop
+// num = 5;
+// for (let i = 1; i <= 10; i++) {
+//   console.log(num * i);
+// }
+
+// Acitvity - 2: while loop
+// Task-3 write the program to print sum of all number between 1 to 10 using while loop
+// let num = 1;
+// let Total = 0;
+// while (num <= 10) {
+//   console.log(num);
+//   Total = Total + num;
+//   num++;
+// }
+// console.log(Total);
+
+// Task - 4 write the program to print 10 to 1 using while loop
+let num = 10;
+let Total = 0;
+while (num >= 1) {
+  console.log(num);
+  num--;
+}
