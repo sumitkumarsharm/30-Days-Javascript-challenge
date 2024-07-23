b15bf3a115a67743e507eab101741dd6424292d0
Sumit0009 <100469632+sumitkumarsharm@users.noreply.github.com>
2024-07-23 20:12:32 +0000

    30 Day's challenge   Day-5

---
+++ Day-5_Function/Function.js
@@ -0,0 +1,13 @@
+// Task/ Activities
+
+// Activity - 1 Function Decelaration
+
+// Task-1 : write the function to check the number is odd or even log it the result to the console
+function Even_Odd(Numbers) {
+  if (Numbers % 2 === 0) {
+    console.log("The number is even");
+  } else {
+    console.log("the number is an Odd");
+  }
+}
+Even_Odd(5);
