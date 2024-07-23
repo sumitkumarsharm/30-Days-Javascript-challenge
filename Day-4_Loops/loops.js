ee86b5954a9d65f20c967a8d9b7f9122a34a08e7
Sumit0009 <100469632+sumitkumarsharm@users.noreply.github.com>
2024-07-23 14:42:16 +0000

    30 Day's Challenge of Day-4

--- Day-4_Loops/loops.js
+++ Day-4_Loops/loops.js
@@ -7,26 +7,46 @@
 // }
 
 // Task-2 write the program to print mulyiplication of 5 using While loop
-// num = 5;
+// num_1 = 5;
 // for (let i = 1; i <= 10; i++) {
-//   console.log(num * i);
+//   console.log(num_1 * i);
 // }
 
 // Acitvity - 2: while loop
 // Task-3 write the program to print sum of all number between 1 to 10 using while loop
-// let num = 1;
+// let num_2 = 1;
 // let Total = 0;
-// while (num <= 10) {
-//   console.log(num);
-//   Total = Total + num;
-//   num++;
+// while (num_2 <= 10) {
+//   console.log(num_2);
+//   Total = Total + num_2;
+//   num_2++;
 // }
 // console.log(Total);
 
 // Task - 4 write the program to print 10 to 1 using while loop
-let num = 10;
-let Total = 0;
-while (num >= 1) {
-  console.log(num);
-  num--;
-}
+// let num_3 = 10;
+// let Total = 0;
+// while (num_3 >= 1) {
+//   console.log(num_3);
+//   num_3--;
+// }
+
+// Activity - 3
+// Task-5 : write the program to print number form 1 to 5 using a do while loop
+// let num_4 = 1;
+// do {
+//   console.log(num_4);
+//   num_4++;
+// } while (num_4 <= 5);
+
+// Task - 6 : write the program to print factorial using do while loop
+// let num_5 = 5;
+// let fact = 1;
+// do {
+//   fact = fact * num_5;
+//   num_5--;
+// } while (num_5 > 1);
+// console.log(fact);
+
+// Activity - 4
+// Task - 7 write the program to print a pattern using nested for loop
