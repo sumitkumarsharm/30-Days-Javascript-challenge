085310311182a8127f14362665dcee52a033f72e
Sumit0009 <100469632+sumitkumarsharm@users.noreply.github.com>
2024-07-23 09:17:53 +0000

    30-Day's Challenge Day 3

---
+++ Day-3_Control_Structure/Control_structures.js
@@ -0,0 +1,114 @@
+// Task & Activities
+// Activity  - 1 IF-Else statment
+
+// Task-1 write the program to check the number positive negative and zero and log the result to the console
+// let Firse_number = 0;
+// if (Firse_number > 0) {
+//   console.log("The number is positive");
+// } else if (Firse_number < 0) {
+//   console.log("The number is negative");
+// } else {
+//   console.log("The number is zero");
+// }
+
+// Task -2  write the program to check the persone is eligible for vote or not and console it to the terminal
+
+// let age = 2;
+// if (age >= 18) {
+//   console.log("You are eligible for vote");
+// } else {
+//   console.log("You are not eligible for vote");
+// }
+
+// Activity -2 Nested if else statments
+//  Task-3 write the program to find largest of three numbers using nested if - else statment
+// let Firse_number_2 = 52;
+// let Second_number_2 = 93;
+// let Third_number_2 = 425;
+
+// if (Firse_number_2 > Second_number_2) {
+//   if (Firse_number_2 > Third_number_2) {
+//     console.log("The largest number is " + Firse_number_2);
+//   } else {
+//     console.log("The largest number is " + Third_number_2);
+//   }
+// } else if (Second_number_2 > Third_number_2) {
+//   console.log("The largest number is " + Second_number_2);
+// } else {
+//   console.log("The largest number is " + Third_number_2);
+// }
+
+// Activity-3 : switch case
+//  Task - 4 write the program that use a switch to determine the day of week based on the number (1-7) and log the day name to the console.
+// let Day = 0;
+
+// switch (Day) {
+//   case 1:
+//     console.log("Sunday");
+//     break;
+//   case 2:
+//     console.log("Monday");
+//     break;
+//   case 3:
+//     console.log("Tuesday");
+//     break;
+//   case 4:
+//     console.log("Wednesday");
+//     break;
+//   case 5:
+//     console.log("Thursday");
+//     break;
+//   case 6:
+//     console.log("Friday");
+//     break;
+//   case 7:
+//     console.log("Saturday");
+//     break;
+//   default:
+//     console.log("Invalid day");
+// }
+
+// Task - 5 write the program to uses a switch case to assign the grade (A,B,C,D,F) based on score and log the grade to the coonsole .
+// let score = 80;
+// switch (score) {
+//   case 90:
+//     console.log("A");
+//     break;
+//   case 80:
+//     console.log("B");
+//     break;
+//   case 70:
+//     console.log("C");
+//     break;
+//   case 60:
+//     console.log("D");
+//     break;
+//   case 50:
+//     console.log("F");
+//     break;
+//   default:
+//     console.log("Invalid score");
+//     break;
+// }
+
+// Activity - Conditionals(ternary) Operator
+// Task - 6 wirte a program that uses the ternary operator to check if a number is even or odd  and log the result to the console.
+// let num = 9;
+// num % 2 === 0 ? console.log("it is even") : console.log("it is odd");
+
+// Activity - 7 Combining Conditions
+// Task - 8 write the program  to chek if the year is a leap year using multiplal consitio (divisible by 4 and not devisibal by 100 and it is also devesible by 400) and log the result to the console
+// let year = 2024;
+// if (year % 400 === 0) {
+//   console.log("LEAP YEAR");
+// } else {
+//   if (year % 4 === 0 && year % 100 !== 0) {
+//     console.log("leap year");
+//   } else {
+//     console.log("not a leap year");
+//   }
+// }
+
+
+// Feature Request 
+// (1) Number check Script 
